1cd7f62cd3303580c2d3795226b992473076fff5
Ivan <aisivan36@gmail.com>
2021-05-28 18:39:04 +0700

    moved materialapp

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,6 @@
 import 'package:flutter/material.dart';
 // import 'package:img_pict/ch2/home.dart';
-import 'package:img_pict/ch3/homech3.dart';
+// import 'package:img_pict/ch3/homech3.dart';
+import 'package:img_pict/ch4/homech4.dart';
 
-void main() => runApp(MyApp());
-
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'A title app',
-      theme: ThemeData(primarySwatch: Colors.blueGrey),
-      home: HomeCh3(),
-    );
-  }
-}
+void main() => runApp(HomeCh4());
